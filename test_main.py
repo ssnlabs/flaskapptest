858ecd731c318b5c744298043a7505992b5d0853
ssnlabs <137744428+ssnlabs@users.noreply.github.com>
2024-11-21 21:17:04 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ class TestApp(unittest.TestCase):
     def test_add(self):
         return self.assertEqual(add(5,3),8)
     def test_sub(self):
-        return self.assertEqual(sub(5,3),2)
+        return self.assertEqual(sub(5,2),3)
 
 
 if __name__=="__main__":
